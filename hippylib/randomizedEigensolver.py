30f0c29aaa9a90f7756e43d9aabb40ef57c617aa
Umberto Villa <umberto.villa@gmail.com>
2017-06-19 16:12:32 -0500

    Switch license to GPLv2

--- hippylib/randomizedEigensolver.py
+++ hippylib/randomizedEigensolver.py
@@ -9,7 +9,7 @@
 #
 # hIPPYlib is free software; you can redistribute it and/or modify it under the
 # terms of the GNU General Public License (as published by the Free
-# Software Foundation) version 3.0 dated June 2007.
+# Software Foundation) version 2.0 dated June 1991.
 
 from dolfin import Vector
 import numpy as np

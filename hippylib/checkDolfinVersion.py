e21e1e8f9239460784bb53b7a78d2016def90f58
Umberto Villa <umberto.villa@gmail.com>
2017-11-08 11:19:34 -0600

    Finalize release 1.4.0

--- hippylib/checkDolfinVersion.py
+++ hippylib/checkDolfinVersion.py
@@ -1,8 +1,15 @@
-'''
-Created on Nov 22, 2016
-
-@author: uvilla
-'''
+# Copyright (c) 2016-2017, The University of Texas at Austin & University of
+# California, Merced.
+#
+# All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the hIPPYlib library. For more information and source code
+# availability see https://hippylib.github.io.
+#
+# hIPPYlib is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 2.0 dated June 1991.
 
 from __future__ import absolute_import, division, print_function
 

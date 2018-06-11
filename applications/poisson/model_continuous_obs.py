83b47eae3418e0bfa2431955371949f52aad03b3
Umberto Villa <umberto.villa@gmail.com>
2018-06-11 16:16:58 -0500

    Fix failed test

--- applications/poisson/model_continuous_obs.py
+++ applications/poisson/model_continuous_obs.py
@@ -13,7 +13,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from dolfin import *
+import dolfin as dl
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -38,15 +38,15 @@ class Poisson:
         self.Vh = Vh
         
         # Initialize Expressions
-        self.mtrue = Expression('log(2 + 7*(pow(pow(x[0] - 0.5,2) + pow(x[1] - 0.5,2),0.5) > 0.2))',
+        self.mtrue = dl.Expression('log(2 + 7*(pow(pow(x[0] - 0.5,2) + pow(x[1] - 0.5,2),0.5) > 0.2))',
                                 element=Vh[PARAMETER].ufl_element())
-        self.f = Constant(1.0)
-        self.u_o = Vector()
+        self.f = dl.Constant(1.0)
+        self.u_o = dl.Vector()
         
-        self.u_bdr = Constant(0.0)
-        self.u_bdr0 = Constant(0.0)
-        self.bc = DirichletBC(self.Vh[STATE], self.u_bdr, u_boundary)
-        self.bc0 = DirichletBC(self.Vh[STATE], self.u_bdr0, u_boundary)
+        self.u_bdr = dl.Constant(0.0)
+        self.u_bdr0 = dl.Constant(0.0)
+        self.bc = dl.DirichletBC(self.Vh[STATE], self.u_bdr, u_boundary)
+        self.bc0 = dl.DirichletBC(self.Vh[STATE], self.u_bdr0, u_boundary)
         
         # Assemble constant matrices      
         self.prior = Prior
@@ -74,18 +74,18 @@ class Poisson:
         If component is STATE, PARAMETER, or ADJOINT return x[component]
         """
         if component == "ALL":
-            x = [Vector(), Vector(), Vector()]
+            x = [dl.Vector(), dl.Vector(), dl.Vector()]
             self.Wuu.init_vector(x[STATE],0)
             self.prior.init_vector(x[PARAMETER],0)
             self.Wuu.init_vector(x[ADJOINT], 0)
         elif component == STATE:
-            x = Vector()
+            x = dl.Vector()
             self.Wuu.init_vector(x,0)
         elif component == PARAMETER:
-            x = Vector()
+            x = dl.Vector()
             self.prior.init_vector(x,0)
         elif component == ADJOINT:
-            x = Vector()
+            x = dl.Vector()
             self.Wuu.init_vector(x,0)
             
         return x
@@ -100,19 +100,19 @@ class Poisson:
         """
         Assemble the matrices and rhs for the forward/adjoint problems
         """
-        trial = TrialFunction(self.Vh[STATE])
-        test = TestFunction(self.Vh[STATE])
+        trial = dl.TrialFunction(self.Vh[STATE])
+        test = dl.TestFunction(self.Vh[STATE])
         m = vector2Function(x[PARAMETER], self.Vh[PARAMETER])
-        Avarf = inner(exp(m)*nabla_grad(trial), nabla_grad(test))*dx
+        Avarf = dl.inner(dl.exp(m)*dl.grad(trial), dl.grad(test))*dl.dx
         if not assemble_adjoint:
-            bform = inner(self.f, test)*dx
-            Matrix, rhs = assemble_system(Avarf, bform, self.bc)
+            bform = dl.inner(self.f, test)*dl.dx
+            Matrix, rhs = dl.assemble_system(Avarf, bform, self.bc)
         else:
             # Assemble the adjoint of A (i.e. the transpose of A)
             u = vector2Function(x[STATE], self.Vh[STATE])
             obs = vector2Function(self.u_o, self.Vh[STATE])
-            bform = inner(obs - u, test)*dx
-            Matrix, rhs = assemble_system(adjoint(Avarf), bform, self.bc0)
+            bform = dl.inner(obs - u, test)*dl.dx
+            Matrix, rhs = dl.assemble_system(dl.adjoint(Avarf), bform, self.bc0)
             
         if assemble_rhs:
             return Matrix, rhs
@@ -123,12 +123,12 @@ class Poisson:
         """
         Assemble the derivative of the forward problem with respect to the parameter
         """
-        trial = TrialFunction(self.Vh[PARAMETER])
-        test = TestFunction(self.Vh[STATE])
+        trial = dl.TrialFunction(self.Vh[PARAMETER])
+        test = dl.TestFunction(self.Vh[STATE])
         u = vector2Function(x[STATE], Vh[STATE])
         m = vector2Function(x[PARAMETER], Vh[PARAMETER])
-        Cvarf = inner(exp(m) * trial * nabla_grad(u), nabla_grad(test)) * dx
-        C = assemble(Cvarf)
+        Cvarf = dl.inner(dl.exp(m) * trial * dl.grad(u), dl.grad(test)) * dl.dx
+        C = dl.assemble(Cvarf)
 #        print ( "||m||", x[PARAMETER].norm("l2"), "||u||", x[STATE].norm("l2"), "||C||", C.norm("linf") )
         self.bc0.zero(C)
         return C
@@ -137,10 +137,10 @@ class Poisson:
         """
         Assemble the misfit operator
         """
-        trial = TrialFunction(self.Vh[STATE])
-        test = TestFunction(self.Vh[STATE])
-        varf = inner(trial, test)*dx
-        Wuu = assemble(varf)
+        trial = dl.TrialFunction(self.Vh[STATE])
+        test = dl.TestFunction(self.Vh[STATE])
+        varf = dl.inner(trial, test)*dl.dx
+        Wuu = dl.assemble(varf)
         Wuu_t = Transpose(Wuu)
         self.bc0.zero(Wuu_t)
         Wuu = Transpose(Wuu_t)
@@ -151,12 +151,12 @@ class Poisson:
         """
         Assemble the derivative of the parameter equation with respect to the state
         """
-        trial = TrialFunction(self.Vh[STATE])
-        test  = TestFunction(self.Vh[PARAMETER])
+        trial = dl.TrialFunction(self.Vh[STATE])
+        test  = dl.TestFunction(self.Vh[PARAMETER])
         p = vector2Function(x[ADJOINT], self.Vh[ADJOINT])
         m = vector2Function(x[PARAMETER], self.Vh[PARAMETER])
-        varf = inner(exp(m)*nabla_grad(trial),nabla_grad(p))*test*dx
-        Wmu = assemble(varf)
+        varf = dl.inner(dl.exp(m)*dl.grad(trial),dl.grad(p))*test*dl.dx
+        Wmu = dl.assemble(varf)
         Wmu_t = Transpose(Wmu)
         self.bc0.zero(Wmu_t)
         Wmu = Transpose(Wmu_t)
@@ -166,25 +166,25 @@ class Poisson:
         """
         Assemble the derivative of the parameter equation with respect to the parameter (Newton method)
         """
-        trial = TrialFunction(self.Vh[PARAMETER])
-        test  = TestFunction(self.Vh[PARAMETER])
+        trial = dl.TrialFunction(self.Vh[PARAMETER])
+        test  = dl.TestFunction(self.Vh[PARAMETER])
         u = vector2Function(x[STATE], self.Vh[STATE])
         m = vector2Function(x[PARAMETER], self.Vh[PARAMETER])
         p = vector2Function(x[ADJOINT], self.Vh[ADJOINT])
-        varf = inner(nabla_grad(p),exp(m)*nabla_grad(u))*trial*test*dx
-        return assemble(varf)
+        varf = dl.inner(dl.grad(p),dl.exp(m)*dl.grad(u))*trial*test*dl.dx
+        return dl.assemble(varf)
 
         
     def computeObservation(self, u_o):
         """
         Compute the synthetic observation
         """
-        mt = interpolate(self.mtrue, Vh[PARAMETER])
+        mt = dl.interpolate(self.mtrue, Vh[PARAMETER])
         x = [self.generate_vector(STATE), mt.vector(), None]
         A, b = self.assembleA(x, assemble_rhs = True)
         
         A.init_vector(u_o, 1)
-        solve(A, u_o, b, "cg", amg_method())
+        dl.solve(A, u_o, b, "cg", amg_method())
         
         # Create noisy data, ud
         MAX = u_o.norm("linf")
@@ -206,7 +206,7 @@ class Poisson:
         Wuudiff = self.Wuu*diff
         misfit = .5 * diff.inner(Wuudiff)
         
-        Rm = Vector()
+        Rm = dl.Vector()
         self.prior.init_vector(Rm,0)
         self.prior.R.mult(x[PARAMETER], Rm)
         reg = .5 * x[PARAMETER].inner(Rm)
@@ -222,12 +222,12 @@ class Poisson:
         A, b = self.assembleA(x, assemble_rhs = True)
         A.init_vector(out, 1)
         if dlversion() <= (1,6,0):
-            solver = PETScKrylovSolver("cg", amg_method())
+            solver = dl.PETScKrylovSolver("cg", amg_method())
         else:
-            solver = PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
+            solver = dl.PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
         solver.parameters["relative_tolerance"] = tol
         solver.set_operator(A)
-        nit = solver.solve(out,b)
+        solver.solve(out,b)
         
 #        print ("FWD", (self.A*out - b).norm("l2")/b.norm("l2"), nit)
 
@@ -240,12 +240,12 @@ class Poisson:
         At.init_vector(out, 1)
         
         if dlversion() <= (1,6,0):
-            solver = PETScKrylovSolver("cg", amg_method())
+            solver = dl.PETScKrylovSolver("cg", amg_method())
         else:
-            solver = PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
+            solver = dl.PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
         solver.parameters["relative_tolerance"] = tol
         solver.set_operator(At)
-        nit = solver.solve(out,badj)
+        solver.solve(out,badj)
         
 #        print ("ADJ", (self.At*out - badj).norm("l2")/badj.norm("l2"), nit)
     
@@ -264,16 +264,16 @@ class Poisson:
         self.prior.init_vector(mg,0)
         C.transpmult(x[ADJOINT], mg)
         if misfit_only == False:
-            Rm = Vector()
+            Rm = dl.Vector()
             self.prior.init_vector(Rm,0)
             self.prior.R.mult(x[PARAMETER], Rm)   
             mg.axpy(1., Rm)
         
-        g = Vector()
+        g = dl.Vector()
         self.prior.init_vector(g,1)
         
         self.prior.Msolver.solve(g, mg)
-        g_norm = sqrt( g.inner(mg) )
+        g_norm = np.sqrt( g.inner(mg) )
         
         return g_norm
         
@@ -300,13 +300,13 @@ class Poisson:
         Solve the incremental forward problem for a given rhs
         """
         if dlversion() <= (1,6,0):
-            solver = PETScKrylovSolver("cg", amg_method())
+            solver = dl.PETScKrylovSolver("cg", amg_method())
         else:
-            solver = PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
+            solver = dl.PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
         solver.set_operator(self.A)
         solver.parameters["relative_tolerance"] = tol
         self.A.init_vector(sol,1)
-        nit = solver.solve(sol,rhs)
+        solver.solve(sol,rhs)
 #        print ("FwdInc", (self.A*sol-rhs).norm("l2")/rhs.norm("l2"), nit)
         
     def solveAdjIncremental(self, sol, rhs, tol):
@@ -314,13 +314,13 @@ class Poisson:
         Solve the incremental adjoint problem for a given rhs
         """
         if dlversion() <= (1,6,0):
-            solver = PETScKrylovSolver("cg", amg_method())
+            solver = dl.PETScKrylovSolver("cg", amg_method())
         else:
-            solver = PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
+            solver = dl.PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
         solver.set_operator(self.At)
         solver.parameters["relative_tolerance"] = tol
         self.At.init_vector(sol,1)
-        nit = solver.solve(sol, rhs)
+        solver.solve(sol, rhs)
 #        print ("AdjInc", (self.At*sol-rhs).norm("l2")/rhs.norm("l2"), nit)
     
     def applyC(self, dm, out):
@@ -358,25 +358,25 @@ class Poisson:
             self.Wmm.mult(dm, out)
             
 if __name__ == "__main__":
-    set_log_active(False)
+    dl.set_log_active(False)
     nx = 64
     ny = 64
-    mesh = UnitSquareMesh(nx, ny)
+    mesh = dl.UnitSquareMesh(nx, ny)
     
-    rank = MPI.rank(mesh.mpi_comm())
-    nproc = MPI.size(mesh.mpi_comm())
+    rank = dl.MPI.rank(mesh.mpi_comm())
+    nproc = dl.MPI.size(mesh.mpi_comm())
         
-    Vh2 = FunctionSpace(mesh, 'Lagrange', 2)
-    Vh1 = FunctionSpace(mesh, 'Lagrange', 1)
+    Vh2 = dl.FunctionSpace(mesh, 'Lagrange', 2)
+    Vh1 = dl.FunctionSpace(mesh, 'Lagrange', 1)
     Vh = [Vh2, Vh1, Vh2]
     
     Prior = LaplacianPrior(Vh[PARAMETER], gamma=1e-8, delta=1e-9)
     model = Poisson(mesh, Vh, Prior)
         
-    m0 = interpolate(Expression("sin(x[0])", element=Vh[PARAMETER].ufl_element()), Vh[PARAMETER])
+    m0 = dl.interpolate(dl.Expression("sin(x[0])", element=Vh[PARAMETER].ufl_element()), Vh[PARAMETER])
     modelVerify(model, m0.vector(), 1e-12, is_quadratic = False, verbose = (rank==0))
 
-    m0 = interpolate(Constant(0.0),Vh[PARAMETER])
+    m0 = dl.interpolate(dl.Constant(0.0),Vh[PARAMETER])
     parameters = ReducedSpaceNewtonCG_ParameterList()
     parameters["rel_tolerance"] = 1e-9
     parameters["abs_tolerance"] = 1e-12
@@ -419,9 +419,9 @@ if __name__ == "__main__":
     
     if nproc == 1:
         xx = [vector2Function(x[i], Vh[i]) for i in range(len(Vh))]
-        plot(xx[STATE], title = "State")
-        plot(exp(xx[PARAMETER]), title = "exp(Parameter)")
-        plot(xx[ADJOINT], title = "Adjoint")
-        plot(vector2Function(model.u_o, Vh[STATE]), title = "Observation")
-        interactive()
+        dl.plot(xx[STATE], title = "State")
+        dl.plot(dl.exp(xx[PARAMETER]), title = "exp(Parameter)")
+        dl.plot(xx[ADJOINT], title = "Adjoint")
+        dl.plot(vector2Function(model.u_o, Vh[STATE]), title = "Observation")
+        dl.interactive()
     

4a2cc6ceb042296963b5b2f2bd83dc05b1bef978
Siddhant Wahal <siddhant@ices.utexas.edu>
2018-12-07 18:30:46 -0600

    Modified travis to use build stages and build matrices

---
+++ ci/run_unittest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ev 
+
+echo $FENICS_VERSION
+
+if [ "$FENICS_VERSION" == "2016.2" ]; then
+
+    PYTHON=python
+    QUAY=quay.io/fenicsproject/stable:2016.2.0 
+elif [ "$FENICS_VERSION" == "2017.2" ]; then
+
+    PYTHON=python3
+    QUAY=quay.io/fenicsproject/stable:2017.2.0 
+fi
+
+DOCKER="docker run --rm $(pwd):/home/fenics/hippylib -w"
+
+${DOCKER} /home/fenics/hippylib/hippylib/test $QUAY "$PYTHON test_gaussian_real_prior.py -v"
+${DOCKER} /home/fenics/hippylib/hippylib/test $QUAY "$PYTHON test_deprecation_warnings.py -v"
+
+

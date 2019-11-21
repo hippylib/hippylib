3912ca18903f8e94b1b06c6c0c80fb1e26ce694d
Umberto Villa <uvilla@wustl.edu>
2019-11-21 13:52:05 -0600

    Update run_unittest.sh

--- ci/run_unittest.sh
+++ ci/run_unittest.sh
@@ -12,9 +12,9 @@ DOCKER="docker run --rm -v $(pwd):/home/fenics/hippylib -w"
 ${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -m unittest discover -v"
 ${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -m unittest discover -v -p 'ptest_*' "
 
-${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -c 'import ..hippylib' && mpirun -n 4 $PYTHON ptest_pointwise_observation.py"
-${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -c 'import ..hippylib' && mpirun -n 2 $PYTHON ptest_multivector.py"
-${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -c 'import ..hippylib' && mpirun -n 2 $PYTHON ptest_randomizedSVD.py"
-${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -c 'import ..hippylib' && mpirun -n 2 $PYTHON ptest_randomizedEigensolver.py"
+${DOCKER} /home/fenics/hippylib $IMAGE "$PYTHON -c 'import hippylib' && mpirun -n 4 $PYTHON hippylib/test/ptest_pointwise_observation.py"
+${DOCKER} /home/fenics/hippylib $IMAGE "$PYTHON -c 'import hippylib' && mpirun -n 2 $PYTHON hippylib/test/ptest_multivector.py"
+${DOCKER} /home/fenics/hippylib $IMAGE "$PYTHON -c 'import hippylib' && mpirun -n 2 $PYTHON hippylib/test/ptest_randomizedSVD.py"
+${DOCKER} /home/fenics/hippylib $IMAGE "$PYTHON -c 'import hippylib' && mpirun -n 2 $PYTHON hippylib/test/ptest_randomizedEigensolver.py"
 
 #${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "mpirun -n 4 $PYTHON -m unittest discover -v -p 'ptest_*' "

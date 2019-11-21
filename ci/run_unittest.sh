feda2420f8e65612c8bbbb5ed279b558eb2b2896
Umberto Villa <uvilla@wustl.edu>
2019-11-21 11:49:10 -0600

    Update run_unittest.sh
    Run ptest in serial and parallel

--- ci/run_unittest.sh
+++ ci/run_unittest.sh
@@ -10,5 +10,6 @@ IMAGE=hippylib/fenics:latest
 DOCKER="docker run --rm -v $(pwd):/home/fenics/hippylib -w"
 
 ${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -m unittest discover -v"
+${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "$PYTHON -m unittest discover -v -p 'ptest_*' "
 
 ${DOCKER} /home/fenics/hippylib/hippylib/test $IMAGE "mpirun -n 4 $PYTHON -m unittest discover -v -p 'ptest_*' "

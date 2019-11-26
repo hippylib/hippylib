7b65b1675c9b60cfbd82738d6f8184337843eebb
Umberto Villa <uvilla@wustl.edu>
2019-11-26 11:33:05 -0600

    Update ptest_qoi.py
    Simplify the description of the prior and the settings of the pde linear solvers.

--- hippylib/test/ptest_qoi.py
+++ hippylib/test/ptest_qoi.py
@@ -31,16 +31,6 @@ def u_boundary(x, on_boundary):
 def v_boundary(x, on_boundary):
     return on_boundary and ( x[0] < dl.DOLFIN_EPS or x[0] > 1.0 - dl.DOLFIN_EPS)
 
-def true_model(Vh, gamma, delta, anis_diff):
-    prior = BiLaplacianPrior(Vh, gamma, delta, anis_diff )
-    noise = dl.Vector()
-    prior.init_vector(noise,"noise")
-    parRandom.normal(1., noise)
-    mtrue = dl.Vector()
-    prior.init_vector(mtrue, 0)
-    prior.sample(noise,mtrue)
-    return mtrue
-
 class TestPointwiseObservation(unittest.TestCase):
     def setUp(self):
         ndim = 2
@@ -65,32 +55,12 @@ class TestPointwiseObservation(unittest.TestCase):
             return dl.exp(m)*dl.inner(dl.nabla_grad(u), dl.nabla_grad(p))*dl.dx - f*p*dl.dx
 
         self.pde = PDEVariationalProblem(self.Vh, pde_varf, bc, bc0, is_fwd_linear=True)
-        self.pde.solver = dl.PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
-        self.pde.solver_fwd_inc = dl.PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
-        self.pde.solver_adj_inc = dl.PETScKrylovSolver(self.mesh.mpi_comm(), "cg", amg_method())
-        self.pde.solver.parameters["relative_tolerance"] = 1e-15
-        self.pde.solver.parameters["absolute_tolerance"] = 1e-20
-        self.pde.solver_fwd_inc.parameters = self.pde.solver.parameters
-        self.pde.solver_adj_inc.parameters = self.pde.solver.parameters
          
         gamma = .1
         delta = .5
-        
-        theta0 = 2.
-        theta1 = .5
-        alpha  = np.pi/4
-        pen = 1e1
-        
-        anis_diff = dl.CompiledExpression(ExpressionModule.AnisTensor2D(), degree = 1)
-        anis_diff.set(theta0, theta1, alpha)
-        mtrue = true_model(self.Vh[PARAMETER], gamma, delta,anis_diff)
 
-        locations = np.array([[0.1, 0.1], [0.1, 0.9], [.5,.5], [.9, .1], [.9, .9]])
-
+        self.prior = BiLaplacianPrior(self.Vh[PARAMETER], gamma, delta)
         
-        self.prior = MollifiedBiLaplacianPrior(self.Vh[PARAMETER], gamma, delta, locations, mtrue, anis_diff, pen)
-        
-    def testVariationalQOI(self):
         GC = GammaCenter()
         marker = dl.MeshFunction("size_t", self.mesh, self.mesh.topology().dim()-1)
         marker.set_all(0)
@@ -101,10 +71,13 @@ class TestPointwiseObservation(unittest.TestCase):
         def qoi_varf( x):
             return dl.avg(dl.exp(x[PARAMETER])*dl.dot( dl.grad(x[STATE]), n) )*dss
 
-        qoi = VariationalQOI(self.Vh,qoi_varf) 
-        rqoi = ReducedQOI(self.pde, qoi)
-        
+        self.qoi = VariationalQOI(self.Vh,qoi_varf) 
 
+        
+    def testVariationalQOI(self):
+
+        rqoi = ReducedQOI(self.pde, self.qoi)
+        
         out = reducedQOIVerify(rqoi, self.prior.mean, eps=np.power(.5, np.arange(20,0,-1)),\
                                                                  plotting = False, verbose = False )
         if self.rank == 0:

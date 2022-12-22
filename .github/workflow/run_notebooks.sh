fdd596555ce964992e53de712e125828b1d47bef
Umberto Villa <uvilla@wustl.edu>
2022-12-21 19:33:00 -0600

    Github actions

---
+++ .github/workflow/run_notebooks.sh
@@ -0,0 +1,9 @@
+PYTHON=python3
+
+
+cd /home/fenics/hippylib/tutorial
+jupyter nbconvert --ExecutePreprocessor.kernel_name="python3" --ExecutePreprocessor.timeout=1200 --to html --execute 1_FEniCS101.ipynb
+jupyter nbconvert --ExecutePreprocessor.kernel_name="python3" --ExecutePreprocessor.timeout=1200 --to html --execute 2_PoissonDeterministic.ipynb
+jupyter nbconvert --ExecutePreprocessor.kernel_name="python3" --ExecutePreprocessor.timeout=1200 --to html --execute 3_SubsurfaceBayesian.ipynb
+jupyter nbconvert --ExecutePreprocessor.kernel_name="python3" --ExecutePreprocessor.timeout=1200 --to html --execute 4_AdvectionDiffusionBayesian.ipynb
+jupyter nbconvert --ExecutePreprocessor.kernel_name="python3" --ExecutePreprocessor.timeout=1200 --to html --execute 5_HessianSpectrum.ipynb
---
+++ .github/workflow/run_unittest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ev 
+
+
+PYTHON=python3
+
+cd /home/fenics/hippylib/hippylib/test
+$PYTHON -m unittest discover -v
+$PYTHON -m unittest discover -v -p 'ptest_*'
+
+mpirun -n 4 $PYTHON ptest_pointwise_observation.py
+mpirun -n 2 $PYTHON ptest_multivector.py
+mpirun -n 2 $PYTHON ptest_randomizedSVD.py
+mpirun -n 2 $PYTHON ptest_randomizedEigensolver.py
+mpirun -n 2 $PYTHON ptest_qoi.py
+mpirun -n 2 $PYTHON ptest_collectives.py
+mpirun -n 2 $PYTHON ptest_numpy2expression.py
+mpirun -n 2 $PYTHON ptest_numpy2meshFunction.py

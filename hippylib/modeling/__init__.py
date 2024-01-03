99b7beb983fbdae75b9ae7993333e217bb44f893
Graham Pash <gtpash96@gmail.com>
2024-01-03 14:20:20 -0600

    add td misfit to init

--- hippylib/modeling/__init__.py
+++ hippylib/modeling/__init__.py
@@ -21,7 +21,7 @@ from .timeDependentVector import VectorTD
 
 from .PDEProblem import PDEProblem, PDEVariationalProblem, TimeDependentPDEVariationalProblem
 from .prior import _Prior, LaplacianPrior, SqrtPrecisionPDE_Prior, BiLaplacianPrior, MollifiedBiLaplacianPrior, GaussianRealPrior, BiLaplacianComputeCoefficients, VectorBiLaplacianPrior
-from .misfit import Misfit, ContinuousStateObservation, DiscreteStateObservation, MultDiscreteStateObservation, MultiStateMisfit, PointwiseStateObservation, MultPointwiseStateObservation
+from .misfit import Misfit, ContinuousStateObservation, DiscreteStateObservation, MultDiscreteStateObservation, MultiStateMisfit, PointwiseStateObservation, MultPointwiseStateObservation, MisfitTD
 from .model import Model
 from .modelVerify import modelVerify
 from .reducedHessian import ReducedHessian, FDHessian

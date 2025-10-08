c1edfd50b44d7582a8d654e9f5ead48d07ff314e
dcluo <dc.luo@outlook.com>
2025-10-08 15:20:40 +1000

    support more operator overloading with tdvec

--- hippylib/test/ptest_tdvector.py
+++ hippylib/test/ptest_tdvector.py
@@ -31,13 +31,42 @@ class TestTimeDependentVector(unittest.TestCase):
         self.mesh = dl.UnitSquareMesh(12, 15)
         self.V = dl.FunctionSpace(self.mesh, "Lagrange", 1)
         self.times = [0.0, 1.0, 2.0]
+
         self.tdvector = hp.TimeDependentVector(self.times)
         self.tdvector.initialize(self.V)
+
+        self.true_scalar_sum = hp.TimeDependentVector(self.times)
+        self.true_scalar_sum.initialize(self.V)
+
+        self.true_scalar_sub = hp.TimeDependentVector(self.times)
+        self.true_scalar_sub.initialize(self.V)
+
+        self.true_scalar_mul = hp.TimeDependentVector(self.times)
+        self.true_scalar_mul.initialize(self.V)
+
+        self.scalar_for_mul = 3.0
+        self.scalar_for_add = 3.0
+        self.scalar_for_sub = 3.0
 
         # load some data into the tdvector
         for i in range(self.tdvector.nsteps):
             tmp_fn = dl.interpolate(dl.Constant(i + 1), self.V)
             self.tdvector.data[i].axpy(1.0, tmp_fn.vector())
+
+            tmp_fn = dl.interpolate(dl.Constant(self.scalar_for_add + i + 1), self.V)
+            self.true_scalar_sum.data[i].axpy(1.0, tmp_fn.vector())
+
+            tmp_fn = dl.interpolate(dl.Constant(i + 1 - self.scalar_for_sub), self.V)
+            self.true_scalar_sub.data[i].axpy(1.0, tmp_fn.vector())
+
+            tmp_fn = dl.interpolate(dl.Constant((i + 1) * self.scalar_for_mul), self.V)
+            self.true_scalar_mul.data[i].axpy(1.0, tmp_fn.vector())
+
+
+
+
+
+    
 
     def test_initialization(self):
         """Test initialization of TimeDependentVector"""
@@ -131,3 +160,277 @@ class TestTimeDependentVector(unittest.TestCase):
             res.vector().axpy(1.0, testfn.vector())
             res.vector().axpy(-1.0, out.data[i])
             assert res.vector().norm("linf") < 1e-2
+
+    def test_add_with_tdvec(self):
+        """Test addition of TimeDependentVector"""
+
+        true_sum = hp.TimeDependentVector(self.times)
+        true_sum.initialize(self.V)
+        true_sum.zero()
+        true_sum.axpy(2.0, self.tdvector)
+
+        tdvec_sum = self.tdvector + self.tdvector
+
+        # First check self.tdvector is unchanged
+        diff_with_original = tdvec_sum.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its sum are the same"
+
+
+        diff = true_sum.copy()
+        diff.axpy(-1.0, tdvec_sum)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in add of two TimeDependentVector: {error_norm}")
+        assert error_norm <= 1e-8, "Sum of two TimeDependentVector is incorrect"
+
+
+
+    def test_sub_with_tdvec(self):
+        """Test addition of TimeDependentVector"""
+
+        tdvec_diff = self.tdvector - self.tdvector
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_diff.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its difference are the same"
+
+        # Check that the difference is as expected (i.e., zero)
+        error_norm = tdvec_diff.norm("linf", "l2")
+        # print(f"Error norm in sub of two TimeDependentVector: {error_norm}")
+        assert error_norm <= 1e-8, "Difference of two TimeDependentVector is incorrect"
+
+
+    def test_mul_with_scalar(self):
+        """Test multiplication of TimeDependentVector with scalar"""
+
+        # Test one order of multiplication
+        tdvec_prod = self.tdvector * self.scalar_for_mul
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_prod.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its product are the same"
+
+        # Check multiplication error
+        diff = self.true_scalar_mul.copy()
+        diff.axpy(-1.0, tdvec_prod)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in mul of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8
+
+        # Test the other way around
+        tdvec_prod = self.scalar_for_mul * self.tdvector
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_prod.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its product are the same"
+
+        # Check multiplication error
+        diff = self.true_scalar_mul.copy()
+        diff.axpy(-1.0, tdvec_prod)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in rmul of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8
+
+
+    def test_add_with_scalar(self):
+        """Test addition of TimeDependentVector with scalar"""
+        # Test sum one way 
+
+        tdvec_sum = self.tdvector + self.scalar_for_add
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_sum.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its sum are the same"
+
+        # Check addition error
+        diff = self.true_scalar_sum.copy()
+        diff.axpy(-1.0, tdvec_sum)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in add of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8
+
+        # Sum the other way
+        tdvec_sum = self.scalar_for_add + self.tdvector
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_sum.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its sum are the same"
+
+        # Check addition error
+        diff = self.true_scalar_sum.copy()
+        diff.axpy(-1.0, tdvec_sum)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in radd of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8
+
+
+    def test_sub_with_scalar(self):
+        """Test addition of TimeDependentVector with scalar"""
+
+        # Subtract one way 
+        tdvec_sub = self.tdvector - self.scalar_for_sub
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_sub.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its subtraction are the same"
+
+        # Check subtraction error 
+        diff = self.true_scalar_sub.copy()
+        diff.axpy(-1.0, tdvec_sub)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in add of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8
+
+
+        # Substract the other way 
+        tdvec_sub = self.scalar_for_sub - self.tdvector
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_sub.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its subtraction are the same"
+
+        # Check subtraction error 
+        diff = self.true_scalar_sub.copy()
+        diff.axpy(-1.0, tdvec_sub)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in add of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8
+
+
+    def test_negation(self):
+        tdvector_neg = -self.tdvector
+        true_neg = hp.TimeDependentVector(self.times)
+        true_neg.initialize(self.V)
+        true_neg.zero()
+        true_neg.axpy(-1.0, self.tdvector)
+
+        # check that self.tdvector is unchanged
+        diff_with_original = tdvector_neg.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its negation are the same"
+
+        # Negation error 
+        diff = true_neg.copy()
+        diff.axpy(-1.0, tdvector_neg)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in negation of TimeDependentVector: {error_norm}")
+        assert error_norm <= 1e-8, "Negation of TimeDependentVector is incorrect"
+
+
+    def test_gather_on_zero(self):
+        """Test gather_on_zero of TimeDependentVector"""
+
+        gathered = self.tdvector.gather_on_zero()
+
+        if self.tdvector.mpi_comm.rank == 0:
+            vec_size = self.tdvector.data[0].size()
+            nsteps = self.tdvector.nsteps
+            assert gathered.shape[0] == vec_size * nsteps
+
+            for i in range(nsteps):
+                # slice for timestep 
+                start_index = i * vec_size
+                end_index = (i + 1) * vec_size
+                time_vec = gathered[start_index:end_index]
+
+                # True value should be i + 1 everywhere based on initialization
+                assert np.allclose(time_vec, i+1)
+
+    def test_iadd_with_tdvec(self):
+        """Test in-place addition of TimeDependentVector"""
+
+        tdvec_copy = self.tdvector.copy()
+        tdvec_copy += self.tdvector
+
+        true_sum = hp.TimeDependentVector(self.times)
+        true_sum.initialize(self.V)
+        true_sum.zero()
+        true_sum.axpy(2.0, self.tdvector)
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_copy.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its sum are the same"
+
+        diff = true_sum.copy()
+        diff.axpy(-1.0, tdvec_copy)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in in-place add of two TimeDependentVector: {error_norm}")
+        assert error_norm <= 1e-8, "In-place sum of two TimeDependentVector is incorrect"
+
+    def test_isub_with_tdvec(self):
+        """Test in-place subtraction of TimeDependentVector"""
+
+        tdvec_copy = self.tdvector.copy()
+        tdvec_copy -= self.tdvector
+
+        # Check that self.tdvector is unchanged
+        diff_with_original = tdvec_copy.copy()
+        diff_with_original.axpy(-1.0, self.tdvector)
+        diff_with_original_norm = diff_with_original.norm("linf", "l2")
+        assert diff_with_original_norm > 1e-2, "self.tdvector and its difference are the same"
+
+        # Check that the difference is as expected (i.e., zero)
+        error_norm = tdvec_copy.norm("linf", "l2")
+        # print(f"Error norm in in-place sub of two TimeDependentVector: {error_norm}")
+        assert error_norm <= 1e-8, "In-place difference of two TimeDependentVector is incorrect"
+
+    def test_iadd_with_scalar(self):
+        """Test in-place addition of TimeDependentVector with scalar"""
+
+        tdvec_copy = self.tdvector.copy()
+        tdvec_copy += self.scalar_for_add
+
+        # Check addition error
+        diff = self.true_scalar_sum.copy()
+        diff.axpy(-1.0, tdvec_copy)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in in-place add of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8, "In-place sum of TimeDependentVector with scalar is incorrect"
+
+
+    def test_isub_with_scalar(self):
+        """Test in-place subtraction of TimeDependentVector with scalar"""
+
+        tdvec_copy = self.tdvector.copy()
+        tdvec_copy -= self.scalar_for_sub
+
+        # Check subtraction error 
+        diff = self.true_scalar_sub.copy()
+        diff.axpy(-1.0, tdvec_copy)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in in-place sub of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8, "In-place difference of TimeDependentVector with scalar is incorrect"
+
+    def test_imul_with_scalar(self):    
+        """Test in-place multiplication of TimeDependentVector with scalar"""
+
+        tdvec_copy = self.tdvector.copy()
+        tdvec_copy *= self.scalar_for_mul
+
+        # Check multiplication error
+        diff = self.true_scalar_mul.copy()
+        diff.axpy(-1.0, tdvec_copy)
+        error_norm = diff.norm("linf", "l2")
+        # print(f"Error norm in in-place mul of TimeDependentVector with scalar: {error_norm}")
+        assert error_norm <= 1e-8, "In-place multiplication of TimeDependentVector with scalar is incorrect"
+
+if __name__ == '__main__':
+    unittest.main()
